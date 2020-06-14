b4f5b59ffce9f16ca0c5d33f36ab2e8ca9e313fe
Naim Sedán <naim.alejandro.sedan@gmail.com>
2020-06-14 14:46:42 +0100

    General names such as 'mybutton' or 'myform' changed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,36 +1,36 @@
 // CONTACT FORM
 
-var myform = $("form#myform");
-myform.submit(function(event){
+var contactForm = $("form#contactForm");
+contactForm.submit(function(event){
 	event.preventDefault();
 
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
   var template_id = "template_oU8C6TYI";
 
-  myform.find("button").text("Sending...");
-  emailjs.sendForm(service_id,template_id,myform[0])
+  contactForm.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,contactForm[0])
   	.then(function(){ 
     	alert("Sent!");
-       myform.find("button").text("Send");
+       contactForm.find("button").text("Send");
     }, function(err) {
        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-       myform.find("button").text("Send");
+       contactForm.find("button").text("Send");
     });
   return false;
 });
 
 
 // TO TOP BUTTON
-mybutton = document.getElementById("toTop");
+toTopButton = document.getElementById("toTop");
 
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    toTopButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    toTopButton.style.display = "none";
   }
 }
 
